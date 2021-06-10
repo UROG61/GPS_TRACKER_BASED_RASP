52a6accf89e7198a5c0c2bf2b76890405f0df660
Hacı Selim Altıntaş <64142757+UROG61@users.noreply.github.com>
2021-06-10 13:39:09 +0300

    Update GPS_data_to_Thinkspeak.py

--- GPS_data_to_Thinkspeak.py
+++ GPS_data_to_Thinkspeak.py
@@ -15,27 +15,27 @@ key = "OM03HL3O0O4XOSNF"  # Thingspeak Write API Key
 msgdata = Message() # Creates a Message Instance
 
 def upload_cloud():
-    temp = get_latitude(msgdata)
-    temp1 = get_longitude(msgdata)
-    params = urllib.parse.urlencode({'field1': temp,'field2': temp1, 'key':key })
-    headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept" : "text/plain"}
-    conn = http.client.HTTPConnection("api.thingspeak.com:80")
-    try:
-        conn.request("POST", "/update", params, headers)
+    temp = get_latitude(msgdata) # gps apı den gelen verileri tutuyoruz.
+    temp1 = get_longitude(msgdata) # 
+    params = urllib.parse.urlencode({'field1': temp,'field2': temp1, 'key':key }) # bu alınan verileri thingspeak channel ına bunları ekliyorum yani pars edioyprum
+    headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept" : "text/plain"} # başlık tanımlıyorum
+    conn = http.client.HTTPConnection("api.thingspeak.com:80") #  thing speak teki server a bağlantı sağlıyoruz.
+    try: # post edebilmek için yani thing speak server ına connection isteği atıyorum. 25 te cevabı response ta tutuyorum.
+        conn.request("POST", "/update", params, headers) 
         response = conn.getresponse()
         print(("Lat:",temp))
         print(("Long:",temp1))
-        print((response.status, response.reason))
-        conn.close()
+        print((response.status, response.reason)) # statu ye bakıyoruz yüklenip yüklenemediğini kontrol ediyorum.
+        conn.close() # connection ı kapatıyorum
     except KeyboardInterrupt:
-         print("Connection Failed")             
+         print("Connection Failed")  # hata mesajını print ediyorum           
              
-if __name__ == "__main__":
-    start_gps_receiver(ser, msgdata)
-    time.sleep(10)
-    ready_gps_receiver(msgdata)
-    while True:
-        upload_cloud()
-        time.sleep(sleep)
+if __name__ == "__main__": # gps reciever ı çalıştırıyor
+    start_gps_receiver(ser, msgdata) # gps ı başlat serial port üzerinden oradaki verileri alıyoruz. gpS API  ztn tanımlamıştık oradan buraya geri gelicek.
+    time.sleep(10) # delay 10 sn 
+    ready_gps_receiver(msgdata) #  veriler alındımı diye kontrol etmiştik gps apı kodumuzda, veriler gelirse uploud cloud u çağırmama ve true döndürmeme vesile oluyor
+    while True:  # delay süremize döngü başlatıyor.
+        upload_cloud() #
+        time.sleep(sleep) #
 
 
