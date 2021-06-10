59dee8cf60ee69e99043e707ec392f880bb0945f
Hacı Selim Altıntaş <64142757+UROG61@users.noreply.github.com>
2021-06-10 17:44:20 +0300

    Update GPS_API.py

--- GPS_API.py
+++ GPS_API.py
@@ -17,17 +17,17 @@ def get_gps_data(serial, dmesg): # porttan gelen veriyi mesaj olarak tanımlıyo
             if strRead[0:6] == '$GPGGA':   #
                 dmesg.msg = pynmea2.parse(strRead) #  okunan  str ın gpgga değeriyse bunu mesaj olarak bastrıyoruz
         else:   #
-            if strRead.find('GGA') > 0: # 0 dan büyükse mesajı bastırıyoruz
+            if strRead.find('GGA') > 0: # 0
                 dmesg.msg = pynmea2.parse(strRead) # 
 
 # API to call start the GPS Receiver
-def start_gps_receiver(serial, dmesg):    # Gps in alıcısının başlayıp başlamadıgını kontrol ediyor ve başlıyor
-    t2 = threading.Thread(target=get_gps_data, args=(serial, dmesg))  #
-    t2.start()    #
+def start_gps_receiver(serial, dmesg):    # Gps in alıcısının başlayıp başlamadıgını kontrol ediyor ve başlıyor. 
+    t2 = threading.Thread(target=get_gps_data, args=(serial, dmesg))  # thread fonksiyonu sayesinde eşzamanlı çalışılabiliyor, target sürekli çalışıyor ve hedefi üstteki def fonksiyonu , "args" dışarıdan bir değişken taşımak için
+    t2.start()    # 
     print("GPS Receiver started")   # 
 
 # API to fix the GPS Revceiver
-def ready_gps_receiver(msg):  # buraya gelen parametre "msg" olarak tanımlandı
+def ready_gps_receiver(msg):  # buraya gelen parametre "msg" olarak tanımlandı.
     print("Please wait fixing GPS .....")  #
     dmesg = msg.msg #
     while(dmesg.gps_qual != 1): #  gps den gelen veriyi bakarak eğer bir hata yoksa gps fix available diye  mesajı yazıyor.
